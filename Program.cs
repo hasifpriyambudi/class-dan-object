42f3a0ff3a689d6dab9e38e7b826c86cbe78acc3
Hasif Priyambudi <hasifpriyambudi13@gmail.com>
2021-03-18 00:41:21 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace praktek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Membuat Objek Taxi
+            Taxi taxi = new Taxi();
+            
+            // Pengesetan Nilai Properties
+            taxi.DriverName = "Jono";
+            taxi.OnDuty = true;
+            taxi.NumPassenger = 10;
+
+            // Pemanggilan Method
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();
+
+            Console.ReadKey();
+        }
+    }
+}
